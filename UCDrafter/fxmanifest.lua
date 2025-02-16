76165df0f56ec38e96be4bd345ae498418bc7104
Dynam69 <mihkel.ludvi@hotmail.com>
2025-02-16 18:06:21 +0200

    Add files via upload

---
+++ UCDrafter/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'adamant'
+game 'gta5'
+
+description 'Sheepdog Developemnt LLC 2023'
+
+version '0.1.0'
+
+files {
+    "data/**/*.meta",
+}
+
+data_file 'VEHICLE_HANDLING_FILE' 'data/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
+
+---Vehicle made by Sheepdog Development LLC 2023 https://discord.gg/sheepdogdev https://sheepdogdev.net 
